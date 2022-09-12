46d3f1f1eda759886d3d7f432203fc8c8d40f781
SzabolcsSemsey <113339405+SzabolcsSemsey@users.noreply.github.com>
2022-09-12 10:22:56 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main ()
+{
+    int n = 42; //integer
+    unsigned int u_n = 10; //pozitive
+    float f = 3.14;
+    double d = 3.141592; //nagyobb pontosság
+    char c = 'a'; // character
+
+    printf("%d %u %f %f %c\n", n, u_n, f, d, c);
+}
